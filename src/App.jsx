906d9d03bd4e4d06e768ce2c1a97aa8e4c528f6a
AlexS997 <alexandru.solomon@yahoo.com>
2025-06-30 10:59:24 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header"
+import Main from "./components/content"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
